30d506aa7caed5b6a52c764ca4093b21c4e57c92
Jerin Andrews <jerinandrews125@gmail.com>
2025-10-12 20:53:59 -0500

    Vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>
+          HWSet WEBSITE
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
